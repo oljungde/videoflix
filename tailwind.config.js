046166c4b3159ad06ce8c21d6d7f670e5099c0bd
Oliver Jung <105112816+oljungde@users.noreply.github.com>
2024-12-10 06:40:14 +0100

    install fonts, configure light/dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 module.exports = {
 	content: ["./src/**/*.{html,ts}"],
 	theme: {
-		extend: {},
+    screens: {
+			456: "456px",
+			576: "576px",
+			768: "768px",
+			992: "992px",
+			1140: "1140px",
+			1440: "1440px",
+		},
+		extend: {
+      fontFamily: {
+				sans: ["Roboto", "sans-serif"],
+			},
+    },
 	},
+  darkMode: "class",
 	plugins: [require("rippleui")],
+  rippleui: {
+		themes: [
+			{
+				themeName: "light",
+				colorScheme: "light",
+			},
+			{
+				themeName: "dark",
+				colorScheme: "dark",
+			},
+		],
+	},
 };
